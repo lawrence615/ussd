e7f29f95dd0eee7201030c3f597f5ec376d4e138
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-01-13 16:52:23 +0300

    working on redirecting the app once a user selects any

--- app/Http/Controllers/UssdController.php
+++ app/Http/Controllers/UssdController.php
@@ -6,12 +6,16 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redirect;
 
 class UssdController extends Controller
 {
     public function index(Request $request)
     {
         $serviceCode = $request->get('serviceCode');
+        $sessionId = $request->get('sessionId');
+        $phoneNumber = $request->get('phoneNumber');
+        $text = $request->get('text');
 
         $remove_hash = explode("#", $serviceCode, -1);
         $filter = array_filter($remove_hash);
@@ -21,27 +25,66 @@ class UssdController extends Controller
         if (array_key_exists(4, $extension)) {
             switch ($extension[4]) {
                 case 1:
+                    $this->redirectToApp($phoneNumber, $sessionId, $this->getServerUrl() . "kplc", $text, $serviceCode);
+                    break;
+                default:
+                    $this->sendResponse("App does not exist", 2);
                     break;
             }
 
         } else {
-            $main_menu = "Welcome to Masharyan Ussd apps. Proceed with any:" . PHP_EOL;
+            if ($text == '') {
+                $main_menu = "Welcome to Masharyan Ussd apps. Proceed with any:" . PHP_EOL;
 
-            $response = $main_menu;
-            $main_menu_items = $this->getAvailableApps();
+                $response = $main_menu;
+                $main_menu_items = $this->getAvailableApps();
 
-            $no = 1;
-            foreach ($main_menu_items as $key => $value):
-                $response .= $no . ":" . $value;
-                $no++;
-            endforeach;
+                $no = 1;
+                foreach ($main_menu_items as $key => $value):
+                    $response .= $no . ":" . $value;
+                    $no++;
+                endforeach;
 
-            $this->sendResponse($response, 1);
+                $this->sendResponse($response, 1);
+            } else {
+                $selected_app = $text;
+
+                switch ($selected_app) {
+                    case 1:
+                        $this->redirectToApp($phoneNumber, $sessionId, $this->getServerUrl() . "kplc", "", $serviceCode);
+                        break;
+                    default:
+                        $this->sendResponse("Invalid selection", 2);
+                        break;
+                }
+
+            }
         }
 
 
-        exit;
+    }
 
+
+    public function redirectToApp($phone, $session_id, $url, $text, $serviceCode)
+    {
+        $qry_str = "?phoneNumber=" . trim($phone) . "&text=" . urlencode($text) . "&sessionId=" . $session_id . "&serviceCode=" . $serviceCode;
+
+//        dd($url . $qry_str);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url . $qry_str);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, '3');
+        $content = trim(curl_exec($ch));
+        curl_close($ch);
+
+        return $content;
+    }
+
+    public function getServerUrl()
+    {
+//        return \Illuminate\Support\Facades\Request::server('SERVER_NAME');
+        return 'http://localhost:8000/ussd/serve/';
     }
 
 
