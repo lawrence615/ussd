b82051e29afe81eac7abf792436e5f5226b2c388
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-01-18 16:39:15 +0300

    disabling and enabling foreign keys before and after inserting data respectively

--- database/seeds/BundlesMenuItemsTableSeeder.php
+++ database/seeds/BundlesMenuItemsTableSeeder.php
@@ -11,6 +11,10 @@ class BundlesMenuItemsTableSeeder extends Seeder
      */
     public function run()
     {
+        Eloquent::unguard();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('bundles_menu_items')->truncate();
+        DB::table('bundles_menu_items')->delete();
         DB::table('bundles_menu_items')->delete();
 
         $bundles_menu_items = [
@@ -61,36 +65,48 @@ class BundlesMenuItemsTableSeeder extends Seeder
             ],
             [
                 'menu_id' => 2,
-                'next_menu_id' => 2,
+                'next_menu_id' => 7,
                 'description' => '150MB + 150 SMS @ Sh50'
             ],
             [
                 'menu_id' => 2,
-                'next_menu_id' => 2,
+                'next_menu_id' => 7,
                 'description' => '60MB + 60 SMS @ Sh30'
             ],
             [
                 'menu_id' => 2,
-                'next_menu_id' => 2,
+                'next_menu_id' => 7,
                 'description' => '35MB + 35 SMS @ Sh20'
             ],
             [
                 'menu_id' => 2,
-                'next_menu_id' => 2,
+                'next_menu_id' => 7,
                 'description' => '15MB + 15 SMS @ Sh10'
             ],
             [
                 'menu_id' => 2,
-                'next_menu_id' => 2,
+                'next_menu_id' => 7,
                 'description' => '7MB + 7 SMS @ Sh5'
             ],
             [
                 'menu_id' => 2,
-                'next_menu_id' => 2,
+                'next_menu_id' => 7,
                 'description' => 'Unsubscribe'
+            ],
+            [
+                'menu_id' => 7,
+                'next_menu_id' => 8,
+                'description' => 'Accept'
+            ],
+            [
+                'menu_id' => 7,
+                'next_menu_id' => 9,
+                'description' => 'Decline'
             ]
         ];
 
         DB::table('bundles_menu_items')->insert($bundles_menu_items);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
