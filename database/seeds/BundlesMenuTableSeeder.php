2877246b4f83be31a62d122c9c29f25c587ca806
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-01-18 16:39:53 +0300

    disabling and enabling foreign keys check before and after inserting data respectively

--- database/seeds/BundlesMenuTableSeeder.php
+++ database/seeds/BundlesMenuTableSeeder.php
@@ -11,6 +11,10 @@ class BundlesMenuTableSeeder extends Seeder
      */
     public function run()
     {
+        Eloquent::unguard();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('bundles_menu')->truncate();
+        DB::table('bundles_menu')->delete();
         DB::table('bundles_menu')->delete();
 
         $bundles_menu = [
@@ -21,36 +25,48 @@ class BundlesMenuTableSeeder extends Seeder
             ],
             [
                 'title' => 'Daily Internet Bundles',
-                'menu_type' => 2,
+                'menu_type' => 1,
                 'is_root' => 0
             ],
             [
                 'title' => '7 Day Bundles',
-                'menu_type' => 2,
+                'menu_type' => 1,
                 'is_root' => 0
             ],
             [
                 'title' => 'Buy 90 Day Bundle',
-                'menu_type' => 2,
+                'menu_type' => 1,
                 'is_root' => 0
             ],
             [
                 'title' => 'Buy for Other Number',
-                'menu_type' => 2,
+                'menu_type' => 1,
                 'is_root' => 0
             ],
             [
                 'title' => 'Okoa Bundles',
-                'menu_type' => 2,
+                'menu_type' => 1,
+                'is_root' => 0
+            ],
+            //this type of menu should pick the selected menu item from the previous step
+            [
+                'title' => 'Subscribe to Daily {{name}}',
+                'menu_type' => 1,
                 'is_root' => 0
             ],
             [
-                'title' => 'Subscribe to Daily',
-                'menu_type' => 2,
+                'title' => 'You have successfully subscribed to {{name}}',
+                'menu_type' => 1,
+                'is_root' => 0
+            ],
+            [
+                'title' => 'Safaricom, The Better option',
+                'menu_type' => 1,
                 'is_root' => 0
             ]
         ];
 
         DB::table('bundles_menu')->insert($bundles_menu);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
