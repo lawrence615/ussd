3be5fa4339a83ed63bdb88f14020e89715a2cddb
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-01-20 09:01:37 +0300

    added in the fillable array

--- app/BundlesMenuItems.php
+++ app/BundlesMenuItems.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class BundlesMenuItems extends Model
 {
     protected $table = 'bundles_menu_items';
-    protected $fillable = ['menu_id', 'next_menu_id', 'description'];
+    protected $fillable = ['id', 'menu_id', 'next_menu_id', 'description'];
 
 
 }
